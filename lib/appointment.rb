25ba6c767acd5c5b3b0e324816bd303f501cc3c9
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-31 04:33:46 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class Appointment
+=======
+def Appointment
+>>>>>>> eea2d7248d91dba6123644e74cc6cca6231960de
   attr_accessor :date, :patient, :doctor
   @@all = []
   def self.all
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,17 @@
 class Patient
+<<<<<<< HEAD
   attr_accessor :name
+=======
+>>>>>>> eea2d7248d91dba6123644e74cc6cca6231960de
   @@all = []
   def self.all
     @@all
   end
   def initialize(name)
+<<<<<<< HEAD
     @name = name
+=======
+>>>>>>> eea2d7248d91dba6123644e74cc6cca6231960de
     @@all << self
   end
   def new_appointment(doctor, date)
